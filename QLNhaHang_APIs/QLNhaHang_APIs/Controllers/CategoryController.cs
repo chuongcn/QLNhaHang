1dcd28e45d7eae4039d02b0913065528800a5f31
Mr Karmar <chuongchung.it@gmail.com>
2023-11-14 14:42:02 +0700

    Add class and migration database

---
+++ QLNhaHang_APIs/QLNhaHang_APIs/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QLNhaHang_APIs.Data;
+using QLNhaHang_APIs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLNhaHang_APIs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _context.Category.ToList();
+        }
+    }
+}
---
+++ QLNhaHang_APIs/QLNhaHang_APIs/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QLNhaHang_APIs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLNhaHang_APIs.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
